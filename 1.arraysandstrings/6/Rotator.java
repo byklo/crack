ce7686aed68c9dc92adcc0e366cc859343993e31
byklo <lo.bryan8@gmail.com>
2016-01-17 17:50:34 -0500

    added linked list node for linked list problems. will have to do all of them in java i think

--- 1.arraysandstrings/6/Rotator.java
+++ 1.arraysandstrings/6/Rotator.java
@@ -23,7 +23,7 @@
 import java.util.List;
 import java.util.ArrayList;
 
-public class Rotator{
+public class Rotator {
 
 	public Rotator() {
 
---
+++ 2.linkedlists/Node.java
@@ -0,0 +1,35 @@
+public class Node {
+
+	private int data;
+	private Node next;
+
+	public Node(int data) {
+		this.data = data;
+		this.next = null;
+	}
+
+	public appendToTail(int data) {
+		Node n = new Node(data);
+		Node tail = this;
+		while (tail.getNext() != null) {
+			tail = tail.getNext();
+		}
+		tail.setNext(n);
+	}
+
+	public Node getNext() {
+		return this.next;
+	}
+
+	public void setNext(Node next) {
+		this.next = next;
+	}
+
+	public int value() {
+		return this.data;
+	}
+
+	public void set(int data) {
+		this.data = data;
+	}
+}
